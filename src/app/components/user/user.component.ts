3e4417ba2c8a6e13014f5a8bd8079b361c4fc48e
Janderson de Almeida <jandersoncoelho@gmail.com>
2025-04-17 20:47:32 -0300

    Refatora a formatação do código HTML e TypeScript nos componentes de usuário e tarefas para melhorar a legibilidade.

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -2,17 +2,15 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 import { User } from './user.model'; // Corrected import path
 
-
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-
 export class UserComponent {
   @Input({ required: true }) user!: User;
-  @Input({ required: true}) selected!: boolean;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter();
 
   get imagePath() {
