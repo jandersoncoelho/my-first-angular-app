c02ab74b5a5e4d36870b773cdcc89ae31ae75ecf
Janderson de Almeida <jandersoncoelho@gmail.com>
2025-05-22 20:25:05 -0300

    Refatora exibição de data no componente de tarefa e registra locale pt-BR na aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+// src/main.ts
+import { bootstrapApplication }    from '@angular/platform-browser';
+import { AppComponent }            from './app/app.component';
+import { registerLocaleData }      from '@angular/common';
+import ptBrLocale                  from '@angular/common/locales/pt';
+import { LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { BrowserModule }           from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// registra pt-BR
+registerLocaleData(ptBrLocale, 'pt-BR');
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ]
+})
+.catch(err => console.error(err));
